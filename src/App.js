96be6cc6e1b169db53989c89064e7362b3fb3f54
Glo_study <3809164@gmail.com>
2022-04-16 14:35:34 +0300

    createComponents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Header from "../src/Components/Header/Header";
 
 const App = () => {
   return (
-      <div className="App">
-        <h1>Hello world!</h1>
+      <div>
+          <Header/>
+          <p>kjsdcbksdjcb</p>
       </div>
-  );
+
+);
 }
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+    <div className={"App-header"}>dskовл оло</div>
+    )
+}
+export default Header
